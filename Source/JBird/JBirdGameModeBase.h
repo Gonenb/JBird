09bfc50f5e6adcca99002c38cf3a367736e4ac22
Gonen <gonenb@hotmail.com>
2017-11-18 17:14:52 +0200

    Initial commit

---
+++ Source/JBird/JBirdGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JBirdGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JBIRD_API AJBirdGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
